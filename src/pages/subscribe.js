4844a5ac44a855aa615e2d23c5984f4fa484b0f7
Awesome Orgs <64356400+awesomeorgsco@users.noreply.github.com>
2020-04-30 17:23:58 +0530

    Update subscribe.js

--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -5,9 +5,11 @@ import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Susbcribe" />
+    <h1> Susbcribe to  x's  paper</h1>
+    <p>Your email</p>
+    <p>checkout element</p>
+    <p>Powered by your favourite payments provider</p>
   </Layout>
 )
 
