410f3f1c3f7af878f7022e42a46287f2efb0fa3e
Awesome Orgs <64356400+awesomeorgsco@users.noreply.github.com>
2020-04-30 17:25:29 +0530

    Update dashboard.js

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,9 +5,9 @@ import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Your stats" />
+    <h1>Your stats</h1>
+    <p>money made, posts, views090maybe , whatever else we need</p>
   </Layout>
 )
 
